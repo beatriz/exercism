a2c62601887002d7c6b5474fddd799ee86d5f2ff
Beatriz Magalhaes <mbeatrizmagalhaes@gmail.com>
2017-11-27 12:20:11 +0000

    csharp reverse string

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        var stringArray = input.ToCharArray();
+        Array.Reverse(stringArray);
+        return new string(stringArray);
+    }
+}
